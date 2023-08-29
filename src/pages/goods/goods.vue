62b1173cd84b340b64b55ca7d389f419984e8a53
ayuanye <Y824301@163.com>
2023-08-29 16:25:20 +0800

    完成商品详情页的服务和地址弹出层

--- src/pages/goods/goods.vue
+++ src/pages/goods/goods.vue
@@ -3,6 +3,10 @@ import { getGoodsByIdAPI } from '@/services/goods'
 import type { GoodsResult } from '@/types/goods'
 import { onLoad } from '@dcloudio/uni-app'
 import { ref } from 'vue'
+// @ts-ignore
+import AddressPanel from './components/AddressPanel.vue'
+// @ts-ignore
+import ServicePanel from './components/ServicePanel.vue'
 // 获取屏幕边界到安全区域距离
 const { safeAreaInsets } = uni.getSystemInfoSync()
 
@@ -36,6 +40,19 @@ const onTapImage = (url: string) => {
     // 图片集合的意思
     urls: goods.value!.mainPictures,
   })
+}
+
+const popup = ref<{
+  // open: (type?: 'top' | 'bottom') => void
+  open: (type?: UniHelper.UniPopupType) => void
+  close: () => void
+}>()
+// 弹出层条件渲染
+const popupName = ref<'address' | 'service'>() //是指定义popupName的值只能是adress或者service
+const openPopup = (name: typeof popupName.value) => {
+  // 修改弹出层名称
+  popupName.value = name
+  popup.value?.open()
 }
 </script>
 
@@ -73,11 +90,11 @@ const onTapImage = (url: string) => {
           <text class="label">选择</text>
           <text class="text ellipsis"> 请选择商品规格 </text>
         </view>
-        <view class="item arrow">
+        <view class="item arrow" @tap="openPopup('address')">
           <text class="label">送至</text>
           <text class="text ellipsis"> 请选择收获地址 </text>
         </view>
-        <view class="item arrow">
+        <view class="item arrow" @tap="openPopup('service')">
           <text class="label">服务</text>
           <text class="text ellipsis"> 无忧退 快速退款 免费包邮 </text>
         </view>
@@ -147,6 +164,12 @@ const onTapImage = (url: string) => {
       <view class="buynow"> 立即购买 </view>
     </view>
   </view>
+
+  <!-- uni-ui 弹出层 -->
+  <uni-popup ref="popup" type="bottom" background-color="#fff">
+    <AddressPanel v-if="popupName === 'address'" @close="popup?.close()" />
+    <ServicePanel v-if="popupName === 'service'" @close="popup?.close()" />
+  </uni-popup>
 </template>
 
 <style lang="scss">
