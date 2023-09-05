a3bd18a55e429736a65c6cd676968f548fde837e
ayuanye <Y824301@163.com>
2023-09-05 17:05:12 +0800

    地址管理页面数据展示、新增和修改完成

---
+++ src/services/address.ts
@@ -0,0 +1,50 @@
+import type { AddressItem, AddressParams } from '@/types/address'
+import { http } from '@/utils/http'
+
+/**
+ * 添加收货地址
+ * @param data 请求参数
+ * @returns
+ */
+export const postMemberAddressAPI = (data: AddressParams) => {
+  return http({
+    method: 'POST',
+    url: '/member/address',
+    data,
+  })
+}
+
+/**
+ * 获取收货地址
+ */
+export const getMemberAddressAPI = () => {
+  return http<AddressItem[]>({
+    method: 'GET',
+    url: '/member/address',
+  })
+}
+
+/**
+ * 获取收货地址详细数据
+ * @param id 地址id（路径参数）
+ */
+export const getMemberAddressByIdAPI = (id: string) => {
+  return http({
+    method: 'GET',
+    url: `/member/address/${id}`,
+  })
+}
+
+/**
+ * 修改收货地址
+ * @param id 点击修改地址信息路径传过来的
+ * @param data 修改地址的form表单数据
+ * @returns
+ */
+export const putMemberAddressByIdAPI = (id: string, data: AddressParams) => {
+  return http<AddressItem>({
+    method: 'PUT',
+    url: `/member/address/${id}`,
+    data,
+  })
+}
